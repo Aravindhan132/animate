875b7d66128d5ef3ec5f49e1207a46931b1faa23
Aravindhan <virartaravindhan@gmail.com>
2018-09-03 14:59:47 +0530

    Source code

---
+++ animate/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  animate
+//
+//  Created by aravind-pt2199 on 03/09/18.
+//  Copyright © 2018 aravind-pt2199. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+       
+        
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
